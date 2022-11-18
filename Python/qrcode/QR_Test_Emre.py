735623435b8f689bf2bf36378336e82da165e9c8
Ferdi Ileri <12001728@student.pxl.Be>
2022-11-18 15:21:22 +0100

    ctrl z

--- Python/qrcode/QR_Test_Emre.py
+++ Python/qrcode/QR_Test_Emre.py
@@ -2,29 +2,28 @@
 
 import cv2
 import webbrowser
-from picamera import PiCamera
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
-cam = PiCamera()
+cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 detector = cv2.QRCodeDetector()
 # Check if camera opened successfully
-if (cam.start_preview() == False):
+if (cam.isOpened() == False):
   print("Error opening video stream or file")
 
 # Read the video
-while(cam.start_preview()):
+while(cam.isOpened()):
     check, frame = cam.read()
     # detect and decode
     data, bbox, _ = detector.detectAndDecode(frame)
     # check if there is a QRCode in the image
     if data:
        a=data
-       b=webbrowser.open(str(a))
        break
     cv2.imshow('QRCODEscanner', frame)
     
     if cv2.waitKey(1) == ord("q"):
         break
+b=webbrowser.open(str(a))
     
 cam.release()
 cv2.destroyAllWindows()
