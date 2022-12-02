ab5df8876b824ddec317473bcce44d2666504434
Ferdi Ileri <12001728@student.pxl.Be>
2022-12-02 15:49:21 +0100

    code werkt: unieke id naar database

--- NFC/sql.py
+++ NFC/sql.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import re
 
 mydb = mysql.connector.connect(
   host="db4free.net",
@@ -10,5 +11,22 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 
-mycursor.execute("INSERT INTO studenten (NFC_ID) VALUES (5)")
+"""mycursor.execute("INSERT INTO studenten (NFC_ID) VALUES (['0xbb', '0x76', '0x7b', '0x2b'])")"""
+
+uid = ['0xbb', '0x76', '0x7b', '0x2b']
+
+mylist = []
+for i in uid:
+  d = int(i, base=16)
+  print(d)
+  mylist.append(d)
+  output_id = str(mylist).strip("[,]")
+  s=re.sub(", ","",output_id)
+  
+
+out_int = int(s)
+print(mylist)
+print(s)
+print(type(out_int))
+print(out_int)
 mydb.commit()
