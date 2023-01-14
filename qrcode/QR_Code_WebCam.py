a94dc24994e41fd5ae882f8f741ef07a1f811586
Nikita Pan <12001870@student.pxl.be>
2023-01-14 20:41:14 +0100

    Repo cleaning

--- qrcode/QR_Code_WebCam.py
+++ qrcode/QR_Code_WebCam.py
@@ -1,8 +1,11 @@
-#bronnen: https://www.geeksforgeeks.org/webcam-qr-code-scanner-using-opencv/
+#https://www.geeksforgeeks.org/webcam-qr-code-scanner-using-opencv/
 
 import cv2
 import webbrowser
+<<<<<<< HEAD
+=======
 import time
+>>>>>>> main
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
 cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -18,14 +21,20 @@ while(cam.isOpened()):
     data, bbox, _ = detector.detectAndDecode(frame)
     # check if there is a QRCode in the image
     if data:
-       print(bbox,data)
+<<<<<<< HEAD
+       a=data
+       break
+    cv2.imshow('QRCODEscanner', frame)
+=======
        webbrowser.open(str(data))
        time.sleep(5)
        #break
     cv2.imshow('QR CODE scanner', frame)
+>>>>>>> main
     
     if cv2.waitKey(1) == ord("q"):
         break
+b=webbrowser.open(str(a))
     
 cam.release()
 cv2.destroyAllWindows()
--- qrcode/cameratest.py
+++
@@ -1,6 +0,0 @@
-from picamera import PiCamera, Color
-
-camera = PiCamera()
-camera.resolution = (640,480)
-
-camera.capture('/home/pxl/Desktop/image.jpg')
