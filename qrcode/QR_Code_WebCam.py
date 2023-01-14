94946eee8be1cb36c91b483e877021a2f6f9fe83
Emre <12001429@student.pxl.be>
2023-01-14 20:55:53 +0100

    fix

--- qrcode/QR_Code_WebCam.py
+++ qrcode/QR_Code_WebCam.py
@@ -1,11 +1,8 @@
-#https://www.geeksforgeeks.org/webcam-qr-code-scanner-using-opencv/
+#bronnen: https://www.geeksforgeeks.org/webcam-qr-code-scanner-using-opencv/
 
 import cv2
 import webbrowser
-<<<<<<< HEAD
-=======
 import time
->>>>>>> main
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
 cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -21,20 +18,14 @@ while(cam.isOpened()):
     data, bbox, _ = detector.detectAndDecode(frame)
     # check if there is a QRCode in the image
     if data:
-<<<<<<< HEAD
-       a=data
-       break
-    cv2.imshow('QRCODEscanner', frame)
-=======
+       print(bbox,data)
        webbrowser.open(str(data))
        time.sleep(5)
        #break
     cv2.imshow('QR CODE scanner', frame)
->>>>>>> main
     
     if cv2.waitKey(1) == ord("q"):
         break
-b=webbrowser.open(str(a))
     
 cam.release()
 cv2.destroyAllWindows()
